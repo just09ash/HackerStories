63dbdd839f2fa61e390464cb8bc2c1a32d2035ab
just09ash <just09ash@gmail.com>
2026-03-17 16:34:17 +0530

    Level 4 Completed: Lists in React

--- hacker-stories/README.md
+++ hacker-stories/README.md
@@ -5,3 +5,4 @@ Level 0: https://gamma.app/docs/Level-0-Welcome-tlhddayibkm060m
 Level 1: https://gamma.app/docs/Level-1-Hello-React-Modern-Version-yy31ceqwgd4hlzp
 Level 2: https://gamma.app/docs/Level-2-Your-First-React-Component-ic5dhqnao0fgcs6
 Level 3: https://gamma.app/docs/Level-3-React-JSX-dlaby1gkdyzd0or
+Level 4: https://gamma.app/docs/Level-4-Lists-in-React-hvmq9k2jlg2vvfz
--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,13 +1,44 @@
 import { useState } from 'react'
 
 function App() {
-  function add2Num(n1,n2) 
-  {
-    return n1 + n2;
-  }
+  const list = [
+    {
+      title: "React",
+      url: "https://react.dev",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }
+  ];
+
   return (
     <>
-      <h1>{add2Num(10,20)}</h1>
+      <div>
+        <h1>My Hacker Stories</h1>
+        {list.map(
+          function (item) {
+            return (
+              <ul key={item.objectID}>
+                <li>
+                  <a href={item.url}>{item.title}</a>
+                </li>
+                <li>{item.author}</li>
+                <li>{item.num_comments}</li>
+                <li>{item.points}</li>
+              </ul>
+            )
+          }
+        )}
+      </div>
     </>
   )
 }
